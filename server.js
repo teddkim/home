2ce7bf7301f737cd0747eafea413aa4edde7fb6f
teddkim <tkim0896@gmail.com>
2018-10-23 22:42:01 -0400

    sending images

--- server.js
+++ server.js
@@ -1,35 +1,48 @@
-var http = require('http')
-    , fs = require('fs')
-    , url = require('url')
-    , port = 8080;
+let http = require('http'),
+    fs = require('fs'),
+    url = require('url'),
+    port = process.env.PORT || 8080;
 
-var server = http.createServer(function (req, res) {
-    var uri = url.parse(req.url)
+let server = http.createServer(function (req, res) {
+    let uri = url.parse(req.url);
 
     switch (uri.pathname) {
         case '/':
-            sendFile(res, 'index.html')
-            break
+            sendFile(res, 'index.html');
+            break;
         case '/index.html':
-            sendFile(res, 'index.html')
-            break
-        case '/images/me2.jpg':
-            sendFile(res,'images/me.jpg')
-            break
+            sendFile(res, 'index.html');
+            break;
+        case 'images/me2.jpg':
+            sendFile(res,'images/me.jpg');
+            break;
+        case 'images/GadgetExplorer.png':
+            sendFile(res, 'images/GadgetExplorer.png');
+            break;
+        case 'images/GadgetExplorer2.png':
+            sendFile(res, 'images/GadgetExplorer2.png');
+            break;
+        case 'images/Pathfind.png':
+            sendFile(res, 'images/Pathfind.png');
+            break;
+        case 'images/Pathfind2.png':
+            sendFile(res, 'images/Pathfind2.png');
+            break;
+        case 'images/BookSmart.png':
+            sendFile(res, 'images/BookSmart.png');
+            break;
         default:
             res.end('404 not found')
     }
-})
-
+});
 server.listen(process.env.PORT || port);
-console.log('listening on 8080')
+console.log('listening on 8080');
 
 // subroutines
 
 function sendFile(res, filename) {
-
     fs.readFile(filename, function (error, content) {
-        res.writeHead(200, {'Content-type': 'text/html'})
+        res.writeHead(200, {'Content-type': 'text/html'});
         res.end(content, 'utf-8')
     })
 
